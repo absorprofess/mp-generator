6c17da908826fcc898e346964a0863984a3f4ec6
absorprofess <absorprofess@outlook.com>
2020-05-06 14:08:38 +0800

    Initial commit

---
+++ src/main/java/com/eoner/mapper/GovUserMapper.java
@@ -0,0 +1,16 @@
+package com.eoner.mapper;
+
+import com.eoner.entity.GovUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 政府用户 Mapper 接口
+ * </p>
+ *
+ * @author pengzhaofeng
+ * @since 2020-05-06
+ */
+public interface GovUserMapper extends BaseMapper<GovUser> {
+
+}
---
+++ src/main/java/com/eoner/service/IGovUserService.java
@@ -0,0 +1,16 @@
+package com.eoner.service;
+
+import com.eoner.entity.GovUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 政府用户 服务类
+ * </p>
+ *
+ * @author pengzhaofeng
+ * @since 2020-05-06
+ */
+public interface IGovUserService extends IService<GovUser> {
+
+}
